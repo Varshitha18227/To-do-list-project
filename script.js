f3cb0f27a3d14695638dbc614d999ebb841b0761
Varshitha18227 <veeravarshitha18@gmail.com>
2026-05-20 13:43:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 script.js
+// script.js
+function addTask() {
+  let taskInput = document.getElementById("taskInput");
+  let taskValue = taskInput.value;
+
+  if (taskValue === "") {
+    alert("Please enter a task");
+    return;
+  }
+
+  let li = document.createElement("li");
+  li.innerHTML = `
+    ${taskValue}
+    <button class="delete-btn" onclick="removeTask(this)">Delete</button>
+  `;
+
+  document.getElementById("taskList").appendChild(li);
+
+  taskInput.value = "";
+}
+
+function removeTask(button) {
+  button.parentElement.remove();
+}
